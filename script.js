9ebf11c85372fbc9e947bae5c1c883917c6159cc
rodri <rodrigoelop1@gmail.com>
2023-11-27 01:56:29 -0500

    v2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 AOS.init();
+function reproducirAudio() {
+    var audio = new Audio('./assets/inicio.mp3');
+    audio.play();
+    setTimeout(function() {
+      window.location.reload(); // Recarga la página actual
+    }, 3900); // Tiempo de espera antes de recargar (ajústalo según la duración del audio)
+}
+function reproducirAudioo() {
+    var audio = new Audio('./assets/empezar.wav');
+    audio.play();
+}
